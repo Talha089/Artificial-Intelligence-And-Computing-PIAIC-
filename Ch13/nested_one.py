aa8b4b9ec122d3001c50cccd8bbd84f26bbc7dab
Talha <talhajavaidmalik@gmail.com>
2020-01-12 09:23:22 +0500

    initial commit

---
+++ Ch13/nested_one.py
@@ -0,0 +1,12 @@
+# Exercise 7
+# Translate this into three lines using nesting:
+# if a == 1 and b == 2 and c == 3: 
+
+a  = 1 
+b = 2 
+c = 3 
+if a == 1:
+    if b == 2:
+        if c == 3:
+            print("Hala do something")
+
---
+++ Ch13/nested_two.py
@@ -0,0 +1,15 @@
+# Exercise 2
+# Translate this into a four-line if and elif:
+# if a == b or c == d:
+ # print("ok")
+
+
+a = 1
+b = 1
+c = 1
+d = 1  
+ 
+if a == b:
+  print("jead")
+elif c == d:    
+  print("ok")
